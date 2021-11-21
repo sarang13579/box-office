1e1e4c3b64ef19cc45eaf85aba1486baefe9df89
Sarang <sarang13579@gmail.com>
2021-11-21 18:36:51 +0530

    Created nav bar and page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div><Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>This is 404 page</Route>
-    </Switch>
+        <Route><div>Not found</div></Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/Pages/Starred.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const starred = () => {
+    return (
+        <div>
+            This is starred page
+        </div>
+    )
+}
+
+export default starred
