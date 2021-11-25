f10bf5b6a72fb5723cea4bbeee06ef4e5599c030
Sarang <sarang13579@gmail.com>
2021-11-25 09:25:49 +0530

    Created input search box and fetched results from TVmaze API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+    const [input, setInput] = useState('');
+
+    const onInputChange = (ev) => {
+        setInput(ev.target.value);
+        // console.log(ev.target.value);
+    };
+
+    const onSearch = () => {
+        // https://api.tvmaze.com/search/shows?q=girls
+
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+            console.log(result);
+        })     // Fetch promise so we need to Await
+    };
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            onSearch();
+        }
+        // console.log(ev.keyCode);
+    };
+
     return (
         <MainPageLayout>
-            This is Home page
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
     )
 }
