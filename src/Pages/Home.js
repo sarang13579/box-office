dbed9aea551c51f046106fefd66003f4c5554698
Sarang <sarang13579@gmail.com>
2021-11-25 10:16:59 +0530

    displayed show results and small fetch refactor done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+
 
 const Home = () => {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
 
     const onInputChange = (ev) => {
         setInput(ev.target.value);
@@ -10,12 +13,22 @@ const Home = () => {
     };
 
     const onSearch = () => {
+
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
+            // console.log(result);
+        })     // Fetch promise so we need to Await
+
+
         // https://api.tvmaze.com/search/shows?q=girls
 
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-            console.log(result);
-        })     // Fetch promise so we need to Await
+        // fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+        //     setResults(result);
+        //     console.log(result);
+        // })     // Fetch promise so we need to Await
     };
+
+
 
     const onKeyDown = (ev) => {
         if (ev.keyCode === 13) {
@@ -24,10 +37,23 @@ const Home = () => {
         // console.log(ev.keyCode);
     };
 
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>No results</div>
+        }
+        if (results && results.length > 0) {
+            return (<div>{results.map(item => (
+                <div key={item.show.id}>{item.show.name}</div>
+            ))}</div>);
+        }
+        return null;
+    }
+
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
         </MainPageLayout>
     )
 }
