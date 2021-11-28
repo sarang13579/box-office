14c8fdc5dbccaa959ec7c9dc15fdd4d8746b40dd
Sarang <sarang13579@gmail.com>
2021-11-28 21:33:53 +0530

    installed styled components and created styles for cards

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom';
+
+import { StyledShowCard } from './ShowCard.styled';
 
 
 const ShowCard = ({ id, image, name, summary }) => {
@@ -8,8 +11,8 @@ const ShowCard = ({ id, image, name, summary }) => {
         : 'No description';
 
     return (
-        <div>
-            <div>
+        <StyledShowCard>
+            <div className="img-wrapper">
                 <img src={image} alt="show" />
             </div>
 
@@ -17,11 +20,11 @@ const ShowCard = ({ id, image, name, summary }) => {
 
             <p>{summaryAsText}</p>
 
-            <div>
+            <div className="btns">
                 <Link to={`/show/${id}`}>Read more</Link>
                 <button type="button">Star me</button>
             </div>
-        </div>
+        </StyledShowCard>
     );
 };
 
