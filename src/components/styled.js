764600ee16eec776e57190b5d2e409e54b612094
Sarang <sarang13579@gmail.com>
2021-12-02 10:22:51 +0530

    created show page components and populated with data

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const FlexGrid = styled.div`
-display:flex;
-justify-content:center;
-flex-wrap:wrap;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
 
 export const SearchCard = styled.div`
@@ -25,7 +25,6 @@ export const SearchCard = styled.div`
     }
   }
 
-
   h1 {
     margin: 10px 0;
     font-size: 21px;
@@ -36,3 +35,21 @@ export const SearchCard = styled.div`
   }
 `;
 
+export const Star = styled.div`
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  background-color: #ffc806;
+  clip-path: polygon(
+    50% 0%,
+    61% 35%,
+    98% 35%,
+    68% 57%,
+    79% 91%,
+    50% 70%,
+    21% 91%,
+    32% 57%,
+    2% 35%,
+    39% 35%
+  );
+`;
