303afb461f765b4354a06ce0397206bd8e956fb9
Sarang <sarang13579@gmail.com>
2021-12-06 06:30:32 +0530

    Updates

---
+++ precache-manifest.5c526e056c27aebf23295b488352ec01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a703f1624a9aa306b7b53553f355505",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "83dcc8cadac858fd59bf",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "76a5b23d84ad4252959c",
+    "url": "/box-office/static/js/2.29967bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.29967bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83dcc8cadac858fd59bf",
+    "url": "/box-office/static/js/main.ad08d221.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
